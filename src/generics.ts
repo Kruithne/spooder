67cec3b1f7ea878660d683eab53c1174246f5a95
Kruithne <kruithne@gmail.com>
2023-03-07 14:21:20 +0000

    feat: implement tryCatchAsync generic

--- src/generics.ts
+++ src/generics.ts
@@ -16,6 +16,20 @@ export function tryCatch<T extends (...args: any[]) => any>(fn: T, ...params: an
 }
 
 /**
+ * Try to run the given async function and catch any errors that are thrown.
+ * @param fn - The function to try and catch.
+ * @param params - The parameters to pass to the function.
+ * @returns An array containing the return value of the function and the error if one was thrown.
+ */
+export async function tryCatchAsync<T extends (...args: any[]) => Promise<any>>(fn: T, ...params: any[]): Promise<[Error | undefined, ReturnType<T>]> {
+	try {
+		return [undefined, await fn(...params)];
+	} catch (e) {
+		return [e, undefined];
+	}
+}
+
+/**
  * For each issue in the given ZodError, print a neatly formatted error message in the format:
  * Config error: {issue.message url} at {issue.path} (e.g Config error: {Invalid url} at {domains[0].source})
  * @param error - The error to print.
@@ -29,5 +43,6 @@ export function printZodError(error: z.ZodError): void {
 
 export default {
 	tryCatch,
+	tryCatchAsync,
 	printZodError
 };
