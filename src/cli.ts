ba366bd23aad4385b3d254afbbc4b738b77a0b74
Kruithne <kruithne@gmail.com>
2023-05-31 16:52:56 +0100

    execute update commands in sequence

--- src/cli.ts
+++ src/cli.ts
@@ -59,23 +59,43 @@ function log(message: string, ...args: unknown[]): void {
 }
 
 const config = await load_config();
-const config_update_command = config.update as string ?? '';
 const config_run_command = config.run as string ?? 'bun run index.ts';
 const config_auto_restart_ms = config.autoRestart as number ?? 5000;
+
+let config_update_commands = [] as string[];
+if (config.update) {
+	if (typeof config.update === 'string')
+		config_update_commands = [config.update]
+	else if (Array.isArray(config.update))
+		config_update_commands = config.update;
+}
 
 async function start_server() {
 	log('start_server');
 
-	if (config_update_command.length > 0) {
-		log('running update command: %s', config_update_command);
-		const update = Bun.spawn(parse_command(config_update_command), {
-			cwd: process.cwd(),
-			stdout: 'inherit',
-			stderr: 'inherit'
-		});
+	if (config_update_commands.length > 0) {
+		log('running %d update commands', config_update_commands.length);
 
-		await update.exited;
-		log('auto update exited with code %d', update.exitCode)
+		for (let i = 0; i < config_update_commands.length; i++) {
+			const config_update_command = config_update_commands[i];
+
+			log('[%d] %s', i, config_update_command);
+
+			const update_proc = Bun.spawn(parse_command(config_update_command), {
+				cwd: process.cwd(),
+				stdout: 'inherit',
+				stderr: 'inherit'
+			});
+
+			await update_proc.exited;
+
+			log('[%d] exited with code %d', i, update_proc.exitCode);
+
+			if (update_proc.exitCode !== 0) {
+				log('aborting update due to non-zero exit code from [%d]', i);
+				break;
+			}
+		}
 	}
 
 	Bun.spawn(parse_command(config_run_command), {
