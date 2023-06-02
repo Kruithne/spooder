e7686105e136d108b0cbfae2f3d9db431652424e
Kruithne <kruithne@gmail.com>
2023-06-02 19:46:00 +0100

    fix output prefix for `panic()`

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { dispatch_report } from "./dispatch";
 
 export async function panic(err_message: string, err?: object) {
-	await dispatch_report('caution: ' + err_message, err);
+	await dispatch_report('panic: ' + err_message, err);
 	process.exit(1);
 }
 
