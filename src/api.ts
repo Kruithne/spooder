d66a6090b083d6f1f63bbf9611e7b9f93e875886
Kruithne <kruithne@gmail.com>
2023-10-12 21:48:30 +0100

    satisfy response constructor

--- src/api.ts
+++ src/api.ts
@@ -318,7 +318,7 @@ export function serve(port: number) {
 	
 		// Content-type/content-length are automatically set for blobs.
 		if (response instanceof Blob)
-			return new Response(response, { status: status_code });
+			return new Response(response as BlobPart, { status: status_code });
 
 		// Status codes can be returned from some handlers.
 		if (return_status_code && typeof response === 'number')
